da43676c2396e2d807a8996b886110c63cf52ffc
mnafis5 <94342046+mnafis5@users.noreply.github.com>
2022-06-10 09:19:54 +0700

    upload config folder of public/lpkp

---
+++ config/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASEURL', 'http://localhost:8080/project_LPK/public');
+
+define('DB_HOST','localhost');
+define('DB_USER' , 'root');
+define('DB_PASS','');
+define('DB_NAME','project_magang');
