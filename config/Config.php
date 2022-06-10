caf4ace9b89b47f193ba9706558c5a80e24d073c
mnafis5 <94342046+mnafis5@users.noreply.github.com>
2022-06-10 09:21:37 +0700

    delete

--- config/Config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('BASEURL', 'http://localhost:8080/project_LPK/public');
-
-define('DB_HOST','localhost');
-define('DB_USER' , 'root');
-define('DB_PASS','');
-define('DB_NAME','project_magang');
