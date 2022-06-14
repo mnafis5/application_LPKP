4df33608ae2a0bed5e081d743409e3cc37fa73b9
mnafis5 <94342046+mnafis5@users.noreply.github.com>
2022-06-14 11:03:33 +0700

    added $data['user'] for pulling data profile
    another change was adding view('partial/index')

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -5,7 +5,10 @@ class Home extends Controller{
     {
         $data['style'] = 'css';
         $data['judul'] = 'Home';
+        $data['user'] = $this->model('Profile_model')->getUserByName();
+        $data['profile'] = $this->model('Profile_model')->getProfileimage();
         $this->view('templates/header',$data);
+        $this->view('partials/navbar',$data);
         $this->view('home/index',$data);
         $this->view('templates/footer');
     }
