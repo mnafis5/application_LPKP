17b53be4a09c8cd603eb675b8ae0c63d421d0aa3
mnafis5 <94342046+mnafis5@users.noreply.github.com>
2022-06-24 09:58:01 +0700

    added a page for displaying the import form

---
+++ app/views/daftar/importData.php
@@ -0,0 +1,9 @@
+<center>
+    <h1>Uploader data siswa</h1>
+
+    <form action="http://localhost:8080/Spreadsheet/" method="post" enctype="multipart/form-data">
+    <b>Import data siswa</b>
+    <input type="file" id="siswa" name="siswa" required>
+    <input type="submit" name="submit" value="upload">
+    </form>
+</center>
