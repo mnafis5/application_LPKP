f688f9d0b511b422aea2e04de5d5374ba16259ce
Muchammad Nafiz Dhiaulhaq <94342046+mnafis5@users.noreply.github.com>
2022-06-28 09:29:28 +0700

    Reconstructed the page

--- app/views/profile/index.php
+++ app/views/profile/index.php
@@ -1,8 +1,10 @@
 
         
+         <?php foreach($data['user'] as $row) : ?>
         <div class="container">
-          <h1>Hi,<?= $_SESSION['nama']; ?>! Selamat datang di dashboard anda</h1>
-        </div>
+          <h1>Hi,<?= $row['username']; ?>! Selamat datang di dashboard anda</h1>
+        </div> 
+        <?php endforeach; ?>
 
 
         
